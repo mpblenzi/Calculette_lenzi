11f26895624a2a8ae929d02e1bd70f3ef643af37
mpblenzi <66097029+mpblenzi@users.noreply.github.com>
2022-08-02 14:37:51 +0200

    tests

--- calcul.test.js
+++ calcul.test.js
@@ -5,5 +5,14 @@ describe(
         test('test 1 + 2 = 3', () => {
             expect(calcul(1+2)).toBe(3)
         })
+        test('test 1 - 2 = -1', () => {
+            expect(calcul(1-2)).toBe(-1)
+        })
+        test('test 1 * 2 = 2', () => {
+            expect(calcul(1*2)).toBe(2)
+        })
+        test('test 2 / 2 = 1', () => {
+            expect(calcul(2/2)).toBe(1)
+        })
     }   
 )
